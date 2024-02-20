014a64ce8e7e5b55cf9bdf5b375e54a3bbef64ee
Mehrdad Farahani <m3hrdadfi@gmail.com>
2024-02-20 17:14:02 +0100

    Change tree.py

--- src/models/tbcc/tree.py
+++ src/models/tbcc/tree.py
@@ -1,27 +1,76 @@
 from javalang.ast import Node
 
 
+class BlockNode(object):
+    def __init__(self, node):
+        self.node = node
+        self.is_str = isinstance(self.node, str)
+        self.token = self.get_token(node)
+        self.children = self.add_children()
+
+    def is_leaf(self):
+        if self.is_str:
+            return True
+        return len(self.node.children) == 0
+
+    def get_token(self, node):
+        if isinstance(node, str):
+            token = node
+        elif isinstance(node, set):
+            token = 'Modifier'
+        elif isinstance(node, Node):
+            token = node.__class__.__name__
+        else:
+            token = ''
+        return token
+
+    def ori_children(self, root):
+        if isinstance(root, Node):
+            if self.token in ['MethodDeclaration', 'ConstructorDeclaration']:
+                children = root.children[:-1]
+            else:
+                children = root.children
+        elif isinstance(root, set):
+            children = list(root)
+        else:
+            children = []
+
+        def expand(nested_list):
+            for item in nested_list:
+                if isinstance(item, list):
+                    for sub_item in expand(item):
+                        yield sub_item
+                elif item:
+                    yield item
+
+        return list(expand(children))
+
+    def add_children(self):
+        if self.is_str:
+            return []
+        logic = ['SwitchStatement', 'IfStatement', 'ForStatement', 'WhileStatement', 'DoStatement']
+        children = self.ori_children(self.node)
+        if self.token in logic:
+            return [BlockNode(children[0])]
+        elif self.token in ['MethodDeclaration', 'ConstructorDeclaration']:
+            return [BlockNode(child) for child in children]
+        else:
+            return [BlockNode(child) for child in children if self.get_token(child) not in logic]
+
+
 def get_token(node):
     token = ''
-    primitivetype = ['int', 'byte', 'short', 'long', 'float', 'double', 'boolean', 'char']
     if isinstance(node, str):
-        if node in primitivetype:
-            token = 'PrimitiveType'
-        elif node.isnumeric():
-            token = 'IntegerLiteralExpr'
-        else:
-            token = 'StringLiteralExpr'
+        token = node
     elif isinstance(node, set):
-        token = 'Modifier'
+        token = 'Modifier'  # node.pop()
     elif isinstance(node, Node):
         token = node.__class__.__name__
-    # print(node.__class__.__name__,str(node))
-    # print(node.__class__.__name__, node)
+
     return token
 
 
-def get_child(root):
-    # print(root)
+def get_children(root):
     if isinstance(root, Node):
         children = root.children
     elif isinstance(root, set):
@@ -33,21 +82,53 @@ def get_child(root):
         for item in nested_list:
             if isinstance(item, list):
                 for sub_item in expand(item):
-                    # print(sub_item)
                     yield sub_item
             elif item:
-                # print(item)
                 yield item
 
-    return list(expand(children))  # list of tree objects
+    return list(expand(children))
 
 
 def get_sequences(node, sequence):
-    token, children = get_token(node), get_child(node)
+    token, children = get_token(node), get_children(node)
     sequence.append(token)
-    # print(len(sequence), token)
     for child in children:
         get_sequences(child, sequence)
+
+    if token in ['ForStatement', 'WhileStatement', 'DoStatement', 'SwitchStatement', 'IfStatement']:
+        sequence.append('End')
+
+
+def get_blocks_v1(node, block_seq):
+    name, children = get_token(node), get_children(node)
+    logic = ['SwitchStatement', 'IfStatement', 'ForStatement', 'WhileStatement', 'DoStatement']
+    if name in ['MethodDeclaration', 'ConstructorDeclaration']:
+        block_seq.append(BlockNode(node))
+        body = node.body
+        for child in body:
+            if get_token(child) not in logic and not hasattr(child, 'block'):
+                block_seq.append(BlockNode(child))
+            else:
+                get_blocks_v1(child, block_seq)
+    elif name in logic:
+        block_seq.append(BlockNode(node))
+        for child in children[1:]:
+            token = get_token(child)
+            if not hasattr(node, 'block') and token not in logic + ['BlockStatement']:
+                block_seq.append(BlockNode(child))
+            else:
+                get_blocks_v1(child, block_seq)
+            block_seq.append(BlockNode('End'))
+    elif name is 'BlockStatement' or hasattr(node, 'block'):
+        block_seq.append(BlockNode(name))
+        for child in children:
+            if get_token(child) not in logic:
+                block_seq.append(BlockNode(child))
+            else:
+                get_blocks_v1(child, block_seq)
+    else:
+        for child in children:
+            get_blocks_v1(child, block_seq)
 
 
 def trans_to_sequences(ast):
@@ -56,7 +137,7 @@ def trans_to_sequences(ast):
     return sequence
 
 
-def check_length(arr, limited_size):
-    if len(arr) > limited_size:
-        return True
-    return False
+def trans_to_blocks(ast):
+    block_seq = []
+    get_blocks_v1(ast, block_seq)
+    return block_seq
